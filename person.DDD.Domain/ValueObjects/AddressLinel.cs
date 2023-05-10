94ad707a8a73f0d8a684ba6fd9464d45791d3736
adryanynfante13 <adryanynfante@gmail.com>
2023-05-10 09:28:03 -0500

    objeto de valor y entidad adress

--- person.DDD.Domain/ValueObjects/AddressLinel.cs
+++ person.DDD.Domain/ValueObjects/AddressLinel.cs
@@ -1,12 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace person.DDD.Domain.ValueObjects
 {
-    internal class AddressLinel
+    public record AddressLinel
     {
+        public AddressLinel() { }
+
+        public string Value { get; init; }
+
+        internal AddressLinel(string value)
+        {
+            Value = value;
+        }
+        public static AddressLinel Create(string value)
+        {
+            validate(value);
+            return new AddressLinel(value);
+        }
+        private static void validate(string value)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException("el valor no puede ser nulo");
+            }
+        }
     }
 }
--- person.DDD.Domain/ValueObjects/AdressPostalCode.cs
+++ person.DDD.Domain/ValueObjects/AdressPostalCode.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace person.DDD.Domain.ValueObjects
 {
-    internal class AdressPostalCode
+    public record AddressPostalCode
     {
+        public AddressPostalCode() { }
+
+        public string Value { get; init; }
+
+        internal AddressPostalCode(string value)
+        {
+            Value = value;
+        }
+        public static AddressPostalCode Create(string value)
+        {
+            validate(value);
+            return new AddressPostalCode(value);
+        }
+        private static void validate(string value)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException("el valor no puede ser nulo");
+            }
+        }
     }
 }
