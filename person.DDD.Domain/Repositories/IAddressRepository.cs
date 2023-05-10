53828dabc705563c18751520184b7d4b4a006410
adryanynfante13 <adryanynfante@gmail.com>
2023-05-10 09:32:21 -0500

    creación de repositorio
    repo de Address

---
+++ person.DDD.Domain/Repositories/IAddressRepository.cs
@@ -0,0 +1,14 @@
+﻿using person.DDD.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace person.DDD.Domain.Repositories
+{
+    public interface IAddressRepository
+    {
+        Task AddAddress(Address ToSave);
+    }
+}
